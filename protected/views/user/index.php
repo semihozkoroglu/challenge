337059a5fc0612a2fd87d603c9b09bfd06f1101b
Semih Özköroğlu <semih.ozkoroglu@bil.omu.edu.tr>
2012-10-08 00:37:33 +0300

    like button

--- protected/views/user/index.php
+++ protected/views/user/index.php
@@ -1,4 +1,15 @@
 <input type=button onClick="location.href='<?php echo $loginUrl?>'" value='Katıl'>
+<br>
+<br>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/tr_TR/all.js#xfbml=1&appId=438883099491250";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
-<iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FChallengeUygulamasiCommunity&amp;send=false&amp;layout=button_count&amp;width=450&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=verdana&amp;height=21&amp;appId=438883099491250" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:450px; height:21px;" allowTransparency="true"></iframe>
+<div class="fb-like" data-href="https://www.facebook.com/ChallengeUygulamasiCommunity" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div>
+
 
